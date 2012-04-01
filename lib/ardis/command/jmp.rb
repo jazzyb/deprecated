5a894c0c494b13f6c9e83a4d570aa671d41f9ef1
jazzyb <json.barnes@gmail.com>
2012-03-31 19:19:24 -0500

    Prepares for handling jump tables.

--- lib/ardis/command/jmp.rb
+++ lib/ardis/command/jmp.rb
@@ -3,11 +3,17 @@ require 'ardis/instruction'
 module Ardis
   module Command
     class Jmp < Instruction
-      NEAR_JUMP_RE = /\A(?<jmp>j\w+)\s+(?<addr>[0-9a-f]+)/
+      NEAR_JUMP_RE = /\A(?<jmp>j\w+\s+)(?<addr>[0-9a-f]+)/
+      JUMP_TABLE_RE = %r{\A(?<jmp>j\w+\s+\*)
+                           (?<offset>0x[0-9a-f]+)
+                           (?<rest>.*)\Z
+      }x
 
       def resolve
         if (md = NEAR_JUMP_RE.match @cmd)
           resolve_near_jump md[:jmp], md[:addr]
+        elsif (md = JUMP_TABLE_RE.match @cmd)
+          resolve_jump_table md[:jmp], md[:offset], md[:rest]
         else
           warn "unrecognized jmp command '#@addr: #@cmd'"
         end
@@ -17,7 +23,7 @@ module Ardis
 
       def resolve_near_jump (jmp, addr)
         if @reloc
-          @cmd = "#{jmp}\t#@reloc"
+          @cmd = "#{jmp}#@reloc"
         else
           i = @section.get_instruction addr
           if i.nil?
@@ -25,6 +31,12 @@ module Ardis
             return
           end
           @cmd = "#{jmp}\t#{i.get_label}"
+        end
+      end
+
+      def resolve_jump_table (jmp, offset, rest)
+        unless @reloc
+          warn "no reloc exists for jmp command '#@addr: #@cmd'"
         end
       end
     end
