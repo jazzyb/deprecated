c75aa08594046df43d7f78d75b4ebd1025ad2a6f
jazzyb <json.barnes@gmail.com>
2012-03-31 19:29:01 -0500

    Fixes potential mov bug.

--- lib/ardis/command/mov.rb
+++ lib/ardis/command/mov.rb
@@ -3,12 +3,15 @@ require 'ardis/instruction'
 module Ardis
   module Command
     class Mov < Instruction
-      MOV_RELOC_RE = /\A(?<mov>mov\w*\s+\$)(?<zero>0x0),(?<rest>.*)\Z/
+      MOV_RELOC_RE = %r{\A(?<mov>mov\w*\s+\$)
+                          (?<offset>0x[0-9a-f]+)
+                          (?<rest>,.*)\Z
+      }x
 
       def resolve
         return unless @reloc
         if (md = MOV_RELOC_RE.match @cmd)
-          @cmd = md[:mov] + "#@reloc," + md[:rest]
+          @cmd = md[:mov] + "(#@reloc+#{md[:offset]})" + md[:rest]
         else
           warn "unhandled mov reloc '#@addr: #@cmd'"
         end
