c7434e316478cba3ce1fc38b42685e93b7847c8d
jazzyb <json.barnes@gmail.com>
2012-09-04 06:44:25 -0500

    Reorgs/cleans-up some of the code.

--- ext/main.c
+++ ext/main.c
@@ -7,13 +7,39 @@
 #include "calypso.h"
 
 
-void init_ruby_vm (void)
+#if 0
+static VALUE test_require (VALUE dummy)
+{
+	return rb_const_get(Calypso, rb_intern("FS"));
+}
+#endif
+
+static void load_cwd_lib (void)
+{
+	VALUE lib, load_path;
+
+	lib = rb_str_new_cstr("./lib");
+	load_path = rb_gv_get("$:");
+	rb_ary_push(load_path, lib);
+}
+
+static void init_ruby_vm (void)
 {
 	ruby_init();
+	load_cwd_lib(); /* TODO remove this once we can install with gem */
 	ruby_init_loadpath();
-	rb_require("./lib/calypso");
+	rb_require("calypso");
 	Calypso = rb_const_get(rb_cObject, rb_intern("Calypso"));
 	Calypso_FS = rb_const_get(Calypso, rb_intern("FS"));
+#if 0
+	Calypso_FS = rb_protect(test_require, 0, &error);
+	if (error) {
+		VALUE err_class;
+		VALUE err = rb_gv_get("$!");
+		err_class = rb_class_path(CLASS_OF(err));
+		printf("%s: %s\n", RSTRING_PTR(err_class), RSTRING_PTR(rb_obj_as_string(err)));
+	}
+#endif
 }
 
 int main (int argc, char **argv)
--- lib/calypso.rb
+++ lib/calypso.rb
@@ -1,63 +1,7 @@
 require 'logger'
-
 $log = Logger.new("/tmp/fuse-test.txt")
 $log.level = Logger::DEBUG
 
 module Calypso
-  class FS
-    def initialize
-      @files = {}
-    end
-
-    def get_size (path)
-      $log.debug { "Calypso::FS: size() called" }
-      return nil unless @files.has_key? path
-      @files[path].size
-    end
-
-    def readdir (path)
-      $log.debug { "Calypso::FS: readdir() called" }
-      @files.keys.map { |filename| filename.sub('/', '') }
-    end
-
-    def create (path)
-      $log.debug { "Calypso::FS: create() called" }
-      @files[path] = ""
-    end
-
-    def open (path)
-      $log.debug { "Calypso::FS: open() called" }
-      @files.has_key?(path) ? true : nil
-    end
-
-    def read (path, size, offset)
-      $log.debug { "Calypso::FS: read() called" }
-      @files[path][offset...(offset+size)]
-    end
-
-    def write (path, buf, size, offset)
-      $log.debug { "Calypso::FS: write() called" }
-      @files[path].insert(offset, buf)
-      size
-    end
-
-    # Singleton implementation:
-    #
-    # We are rolling our own singleton pattern below instead of using the
-    # built-in ruby implementation because calling the instance method from our
-    # C code results in the following error:
-    #
-    #   undefined method `synchronize' for #<Mutex:0x847130c>
-    #
-    # I don't know why this error occurs, but since I don't think we need a
-    # mutex for our particular implementation, we should be fairly safe rolling
-    # our own.
-    @@_instance = FS.new
-
-    def self.instance
-      @@_instance
-    end
-
-    private_class_method :new
-  end
+  autoload :FS, 'calypso/fs'
 end
