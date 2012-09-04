74a5aa692345b94e0edad1aace5fc99485c600ba
jazzyb <json.barnes@gmail.com>
2012-09-04 06:56:56 -0500

    More clean-up; removes dead code.

--- ext/main.c
+++ ext/main.c
@@ -7,20 +7,10 @@
 #include "calypso.h"
 
 
-#if 0
-static VALUE test_require (VALUE dummy)
-{
-	return rb_const_get(Calypso, rb_intern("FS"));
-}
-#endif
-
 static void load_cwd_lib (void)
 {
-	VALUE lib, load_path;
-
-	lib = rb_str_new_cstr("./lib");
-	load_path = rb_gv_get("$:");
-	rb_ary_push(load_path, lib);
+	VALUE load_path = rb_gv_get("$:");
+	rb_ary_push(load_path, rb_str_new_cstr("./lib"));
 }
 
 static void init_ruby_vm (void)
@@ -31,15 +21,6 @@ static void init_ruby_vm (void)
 	rb_require("calypso");
 	Calypso = rb_const_get(rb_cObject, rb_intern("Calypso"));
 	Calypso_FS = rb_const_get(Calypso, rb_intern("FS"));
-#if 0
-	Calypso_FS = rb_protect(test_require, 0, &error);
-	if (error) {
-		VALUE err_class;
-		VALUE err = rb_gv_get("$!");
-		err_class = rb_class_path(CLASS_OF(err));
-		printf("%s: %s\n", RSTRING_PTR(err_class), RSTRING_PTR(rb_obj_as_string(err)));
-	}
-#endif
 }
 
 int main (int argc, char **argv)
